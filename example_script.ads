d715f7be95fd5441b2b819bf26d074ef7d680721
Limuran <strelgl2@gmail.com>
2023-06-18 16:58:31 +1000

    Пример кода Ады

---
+++ example_script.ads
@@ -0,0 +1,25 @@
+procedure print(s: String)
+is
+begin
+    Ada.Text_IO.Put_Line(s);
+end print;
+
+function rangeSum(rangeStart: Integer; rangeEnd: Integer) return Integer
+is
+begin
+    s := 0;
+    for i in start .. end loop
+        s = s + i;
+    end loop;
+    return s;
+end rangeSum;
+
+procedure main() is begin
+    if 1 + 2 = 5 and true or not false then
+        print("Helloworld!");
+    elsif false then
+        print("Godbyeworld?");
+    else
+        print(rangeSum(100, 1000));
+    end if;
+end main;
